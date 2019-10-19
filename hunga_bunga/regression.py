937da1297c939358e2ffc10464cac20c336ce66c
Navaneesh Gangala <navaneesh.ds@gmail.com>
2019-10-19 12:48:43 +0530

    Updated regression for compatilibility

--- hunga_bunga/regression.py
+++ hunga_bunga/regression.py
@@ -21,8 +21,8 @@ from sklearn.ensemble import AdaBoostRegressor, ExtraTreesRegressor, RandomFores
 from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet, Lars, LassoLars, OrthogonalMatchingPursuit, BayesianRidge, ARDRegression, SGDRegressor, PassiveAggressiveRegressor, RANSACRegressor, HuberRegressor
 
 
-from core import *
-from params import *
+from hunga_bunga.core import *
+from hunga_bunga.params import *
 
 
 linear_models_n_params = [
